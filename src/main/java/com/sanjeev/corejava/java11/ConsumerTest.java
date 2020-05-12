9131b846852a0bd3e443b17faf0e3bf7a9a31fe3
rajanalasanjeev <rajanala.sanjeev@gmail.com>
2020-05-12 12:12:35 -0400

    Old Files clean up

--- src/main/java/com/sanjeev/corejava/java11/ConsumerTest.java
+++ src/main/java/com/sanjeev/corejava/java11/ConsumerTest.java
@@ -1,10 +1,17 @@
 package com.sanjeev.corejava.java11;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
+//PCSF Predicate (Test), Consumer(accept), Supplier(get), Function (apply)
 public class ConsumerTest {
 
     public static void main(String[] args) {
+        Function<String, String> function = s -> {
+            return s.toUpperCase();
+        };
+        function.apply("sanjeev");
+
         Consumer<String>  consumer = s -> {
             System.out.println(s);
         };
